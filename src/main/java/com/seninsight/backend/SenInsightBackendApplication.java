db4eb12c95a26d5ed434d9ceb75edf3a82e6c326
Dieynaba Fily  Bodiang <dfbodiang@gainde2000.sn>
2026-08-21 18:59:24 +0000

    first commit

---
+++ src/main/java/com/seninsight/backend/SenInsightBackendApplication.java
@@ -0,0 +1,13 @@
+package com.seninsight.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenInsightBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenInsightBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seninsight/backend/SenInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seninsight.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenInsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
